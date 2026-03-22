ff5aa96be57ab6ad4362cf7063728f88b2fe0018
Shaina Dawson <shainadawson@Shainas-MacBook-Air.local>
2026-03-22 01:24:50 -0400

    feat: implement discount logic and update final total in DOM

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,17 @@ let firstItem = parseFloat(prompt("What is the cost of the first item?", "Ex: 2.
 //prompting the user for the cost of the second item
 let secondItem = parseFloat(prompt("What is the cost of the second item?", "ex: 4.00"));
 
+let total = firstItem + secondItem;
+
+if (total >= 100) {
+    let finalCost = 0.90 * total;
+    document.querySelector("#finalTotal").innerHTML = `Your total is ${finalCost.toFixed(2)} which includes a 10% discount`;
+    document.querySelector("#finalTotal").className = "firstclass";
+}
+else if (total >= 50 && total < 100) {
+    let finalCost = 0.95 * total;
+    document.querySelector("#finalTotal").innerHTML = `Your total is ${finalCost.toFixed(2)} which includes a 5% discount`;
+    document.querySelector("#finalTotal").className = "firstclass";
+}
+
+
